e4aa59c678e6d3a7920a18fdf20f962b3a4486ff
fukuro <Elbar_israilov@mail.ru>
2015-11-10 13:01:57 +0600

    Added a header file for storing structure

---
+++ struct.h
@@ -0,0 +1,22 @@
+//
+// Created by root on 11/10/15.
+//
+
+#ifndef LABS_STRUCT_H
+#define LABS_STRUCT_H
+
+struct bank
+{
+    char surname[20];
+    char name[10];
+    char lastname[15];
+    int accountNumber;
+    int account;
+    double day;
+    double month;
+    int year;
+};
+
+
+
+#endif //LABS_STRUCT_H

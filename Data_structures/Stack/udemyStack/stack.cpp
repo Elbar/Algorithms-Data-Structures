5254e070911fadc6644070c721432848e661d863
fukuro <Elbar_israilov@mail.ru>
2015-11-11 16:09:50 +0600

    Simple example of Stack

---
+++ Data_structures/Stack/udemyStack/stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 11/11/15.
+//
+
+#include "stack.h"
---
+++ Data_structures/Stack/udemyStack/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by root on 11/11/15.
+//
+
+#ifndef LABS_STACK_H
+#define LABS_STACK_H
+
+#endif //LABS_STACK_H

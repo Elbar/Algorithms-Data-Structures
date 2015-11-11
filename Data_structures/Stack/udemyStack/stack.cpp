9735c4d7d6f5e8be1bc566836b963d13fdb3282c
fukuro <Elbar_israilov@mail.ru>
2015-11-11 16:54:09 +0600

    Example of stack

--- Data_structures/Stack/udemyStack/stack.cpp
+++ Data_structures/Stack/udemyStack/stack.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "stack.h"
+
+#include <iostream>
+#include <stack>
+
+
+using namespace std;
+
+int main()
+{
+    stack<int> myStack;
+
+
+    myStack.push(5);
+    myStack.push(3);
+    myStack.push(2);
+
+
+    cout<<"The number of ints on the stack"<<myStack.size()<<endl;
+
+
+    while(!myStack.empty()){
+    cout<<"popping "<<myStack.top()<<endl;
+    myStack.pop();
+    }
+    cout<<"The number of ints on the stack"<<myStack.size()<<endl;
+
+
+}

b01838f3828e7851025c2cd03283c7bb53660959
fukuro <Elbar_israilov@mail.ru>
2015-11-24 20:43:45 +0600

    swap method & template

---
+++ Templates/template.cpp
@@ -0,0 +1,41 @@
+//
+// Created by root on 11/24/15.
+//
+
+#include<iostream>
+
+
+using namespace std;
+
+template<class t>
+
+void swap(t *x,t *y)
+{
+    t temp;
+    temp = x;
+    x=y;
+    y=temp;
+}
+
+void fun(int a,int b,float c,float d)
+{
+    cout<<"\na and b before swaping :"<<a<<"\t"<<b;
+    swap(a,b);
+    cout<<"\na and b after swaping  :"<<a<<"\t"<<b;
+    cout<<"\n\nc and d before swaping :"<<c<<"\t"<<d;
+    swap(c,d);
+    cout<<"\nc and d after swaping  :"<<c<<"\t"<<d;
+}
+
+int main()
+{
+    int a,b;
+    float c,d;
+
+    cout<<"Enter A,B values(integer):";
+    cin>>a>>b;
+    cout<<"Enter C,D values(float):";
+    cin>>c>>d;
+    fun(a,b,c,d);
+
+}
